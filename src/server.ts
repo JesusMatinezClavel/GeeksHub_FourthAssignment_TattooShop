e76eebe65beda2e9a52626aec68dcb448852010f
JesusMartinezClavel <jmcvalles@gmail.com>
2024-02-26 19:40:25 +0100

    UPDATE: refactorize the .ts to be cleaner

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,13 @@
-// Importamos el package Express, junto con el Request y Response
-import express, { Application } from "express"
-import { Request, Response } from "express";
-
 // Importamos la config de dotenv
 import 'dotenv/config'
 
-// Se importan automáticamente desde sus respectivos .ts en la carpeta ./src/Controllers
-import { createRoles, deleteRoles, getRoles, updateRoles } from "./Controllers/rolesControllers";
-import { createUsers, deleteUsers, getUsers, updateUsers } from "./Controllers/usersControllers";
-
-// Creamos la constante App a partir de express
-const app: Application = express()
-
-// Creamos el middleware de Express para manejar los datos como .json
-app.use(express.json());
-
 // Creamos la constante PORT con el package dotnet para no subir al repositorio el puerto al que nos conectamos
 const PORT = process.env.PORT || 4001
+
+// importamos app desde app.ts
+import { app } from "./app"
 
 // Ponemos en marcha el servidor
 app.listen(PORT, () => {
     console.log(`the server is up on port ${PORT}`)
 })
-
-// Comprobamos si el servidor responde correctamente
-app.get("/healthy", (req: Request, res: Response) => {
-    res.status(200).json({
-        serverUp: true,
-        message: `the server is healthy`
-    })
-})
-
-
-///////////////////////////////////////////// Roles routes
-
-app.get('/roles', getRoles)
-app.post('/roles', createRoles)
-app.put('/roles', updateRoles)
-app.delete('/roles', deleteRoles)
-
-////////////////////////////////////////////// User routes
-
-app.get('/users', getUsers)
-app.post('/users', createUsers)
-app.put('/users', updateUsers)
-app.delete('/users', deleteUsers)
