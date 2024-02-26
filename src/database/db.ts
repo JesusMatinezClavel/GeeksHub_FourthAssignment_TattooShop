b04a05a07f6a9bab2d6ec1a23232a6f7ad4cdbe2
JesusMartinezClavel <jmcvalles@gmail.com>
2024-02-26 20:44:08 +0100

    FEAT:Created the users table and the scripts for run/revert the migrations

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,6 +2,7 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Roles1708974400667 } from "./migrations/1708974400667-roles"
 import 'dotenv/config'
+import { Users1708975838186 } from "./migrations/1708975838186-users"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -11,7 +12,7 @@ export const AppDataSource = new DataSource({
     password: process.env.DB_password || "",
     database: process.env.DB_database || "test",
     entities: [],
-    migrations: [Roles1708974400667],
+    migrations: [Roles1708974400667,Users1708975838186],
     synchronize: false,
     logging: false
 })
