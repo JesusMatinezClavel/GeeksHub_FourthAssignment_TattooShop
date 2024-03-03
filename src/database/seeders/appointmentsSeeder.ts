e41c0b8c4a88c6849280acb74ee8667ef6602091
JesusMartinezClavel <jmcvalles@gmail.com>
2024-03-03 20:04:56 +0100

    UPDATE: Added more annotations

--- src/database/seeders/appointmentsSeeder.ts
+++ src/database/seeders/appointmentsSeeder.ts
@@ -3,31 +3,37 @@ import { faker } from "@faker-js/faker";
 import { Service } from "../../models/Service";
 import { User } from "../../models/User";
 
+// Generamos una cita aleatoria con el package faker
 export const generateAppointment = () => {
 
     const randomAppointment = new Appointment()
     const startDate = faker.date.soon()
     const endDate = faker.date.future()
+    // Utilizamos startDate y endDate para generar una desde los próximos dias hasta dentro de un año
     randomAppointment.appointmentDate = faker.date.between({ from: startDate, to: endDate })
+    // Generamos un user_id y service_id asignando números aleatorios
     randomAppointment.user = {
         id: faker.number.int({ min: 1, max: 20 })
-    } as User
+    } as User // <-------- Tenemos que indicarle que este valor hace referencia al User de la entidad Appointment
     randomAppointment.service = {
         id: faker.number.int({ min: 1, max: 5 })
-    } as Service
+    } as Service // <-------- Tenemos que indicarle que este valor hace referencia al Service de la entidad Appointment
     return randomAppointment
 
 }
 
+// Exportamos la función del seeder de las citas para llamarla junto con el resto de seeders
 export const seederAppointments = async () => {
 
+    // Importamos el generador de citas aleatorio
     generateAppointment()
-
+    // Creamos un array con 20 citas
     const appointments = Array.from({ length: 20 }, generateAppointment)
 
     await Appointment.save(appointments);
 
 
+    // Mandamos un mensaje por consola para confirmar que las citas se han creado correctamente
     console.log(`---------------------------`);
     console.log(`Appointments have been generated succesfully!`);
     console.log(`---------------------------`);
--- src/database/seeders/seeder_DB.ts
+++ src/database/seeders/seeder_DB.ts
@@ -1,30 +1,31 @@
 import { AppDataSource } from "../db";
-import { generateAppointment, seederAppointments } from "./appointmentsSeeder";
+import { seederAppointments } from "./appointmentsSeeder";
 import { seederRoles } from "./roleSeeder";
 import { seederServices } from "./serviceSeeder";
 import { generateControlUsers, seederUsers } from "./userSeeder";
 
+// Creamos una función para llamar a todos los seeders por orden con el then/catch y así evitar problemas con las FK
 const seedDB = async () => {
 
-        await AppDataSource.initialize()
-            .then(() => seederRoles())
+    // Iniciamos la conexción con la base de datos
+    await AppDataSource.initialize()
+        .then(() => seederRoles())
 
-            .then(() => seederServices())
+        .then(() => seederServices())
 
-            .then(()=> generateControlUsers())
+        .then(() => generateControlUsers())
 
-            .then(() => seederUsers())
+        .then(() => seederUsers())
 
-            .then(() => seederAppointments())
+        .then(() => seederAppointments())
 
-            .catch((error) => {
-                console.log(error)
-            })
-            .finally(() => {
-                AppDataSource.destroy()
-            })
+        .catch((error) => {
+            console.log(error)
+        })
+        .finally(() => {
+            AppDataSource.destroy()
+        })
 }
 
-
-
+// Llamamos a la función con el comando 'npm run seed-db' configurado en el package.json
 seedDB()
